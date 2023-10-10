546b829d5167aac54affd398ba84e579e8f22d47
柏倉希奈 <>
2023-10-10 13:34:23 +0900

    新規作成

--- Bulletin-board-master/app/Http/Controllers/Auth/Login/LoginController.php
+++ Bulletin-board-master/app/Http/Controllers/Auth/Login/LoginController.php
@@ -8,4 +8,9 @@ use Illuminate\Http\Request;
 class LoginController extends Controller
 {
     //
+    public function hello()
+    {
+        echo 'hello world!<br>';
+        echo 'コントローラーを使ったルーティング成功です。';
+    }
 }
