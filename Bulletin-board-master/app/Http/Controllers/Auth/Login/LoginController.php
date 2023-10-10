6ba098831ca9c793ba67d076f62da987daa114e9
柏倉希奈 <>
2023-10-11 01:02:16 +0900

    ログインコントローラー追加

--- Bulletin-board-master/app/Http/Controllers/Auth/Login/LoginController.php
+++ Bulletin-board-master/app/Http/Controllers/Auth/Login/LoginController.php
@@ -13,4 +13,19 @@ class LoginController extends Controller
         echo 'hello world!<br>';
         echo 'コントローラーを使ったルーティング成功です。';
     }
+
+    public function loginView()
+    {
+        return view('auth.login');
+    }
+
+    public function loginPost(Request $request)
+    {
+        $userdata = $request -> only('mail_address', 'password');
+        if (Auth::attempt($userdata)) {
+            return redirect('/top');
+        }else{
+            return redirect('/login')->with('flash_message', 'name or password is incorrect');
+        }
+    }
 }
