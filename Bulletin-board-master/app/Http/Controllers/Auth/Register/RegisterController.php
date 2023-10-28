b5f86ebfe23577376928f4746aea0523e1acd66e
柏倉希奈 <>
2023-10-28 14:22:19 +0900

    新規会員登録機能追加

--- Bulletin-board-master/app/Http/Controllers/Auth/Register/RegisterController.php
+++ Bulletin-board-master/app/Http/Controllers/Auth/Register/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth\Register;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Users\User;
 
 class RegisterController extends Controller
 {
@@ -12,4 +13,20 @@ class RegisterController extends Controller
     {
         return view('auth.register');
     }
+
+    public function registerPost(Request $request)
+    {
+
+            $user_get = User::create([
+                'username' => $request->username,
+                'email' => $request->email,
+                'password' => bcrypt($request->password)
+            ]);
+            //dd($user_get);
+
+
+            $user = User::findOrFail($user_get->id);
+            return redirect()->route('loginView');
+        }
+
 }
